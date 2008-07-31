b19c4ba1affa652b4fd5109f13d7a56c358a46ed
Ugo Riboni <nerochiaro@neurostechnology.com>
2008-07-31 06:16:58 +0100

    Improve vlc helper, avoid unneeded configures
    The helper always called configure script, even when not needed.
    With this helper, we make it just run it when necessary.
    It also allow to just install or clean.

--- scripts/vlc-build-helper.sh
+++ scripts/vlc-build-helper.sh
@@ -1,7 +1,49 @@
+set -e
+
 if [ -z "${PRJROOT}" ] ; then 
    echo "Please source neuros-env from your project root before running this script."
    exit 1
 fi
+
+install_vlc()
+{
+ echo "####### INSTALLING VLC AND CLEANING CRUFT ########"
+
+ make DESTDIR=`pwd`/staging install
+ find staging -name *.so -exec arm-linux-strip --strip-unneeded \{\} \;
+ find staging -name *.a | xargs rm -f;
+ arm-linux-strip --strip-unneeded staging/usr/bin/vlc
+ rm -rf staging/usr/share
+
+ cp -a staging/* ${INSTALL_MOD_PATH}/
+}
+
+clean_vlc()
+{
+ make clean
+}
+
+
+# If requested, just do the installation to rootfs from previously built code.
+# This fails if we didn't build previously.
+if [ "$1" == "install" ] ; then
+   if [ ! -d vlc/vlc-build-davinci ] ; then
+      echo "You need fist to build to be able to install."
+      echo "Run this script with no arguments to build and install."
+      exit 1
+   fi
+   cd vlc/vlc-build-davinci
+   install_vlc
+   exit 0
+fi
+# If requested, just do the cleanup and exit.
+if [ "$1" == "clean" ] ; then
+   echo "####### CLEANING VLC BUILD ########"
+   rm -rf vlc/vlc-build-davinci
+   exit 0
+fi
+
+# Otherwise build and install
 
 # Build all the contrib libraries. This will download a lot of stuff the first time,
 # so be patient.
@@ -9,7 +51,11 @@ fi
 echo "####### BUILDING VLC DEPENDENCIES ########"
 
 cd vlc/extras/contrib
-./bootstrap arm-linux davinci
+if [ ! -f Makefile ] ; then
+   echo "++++++++++++++++++++++++++ NO CONFIGURE +++++++++++++++++++++++"
+   ./bootstrap arm-linux davinci
+   exit 0
+fi
 make
 cd ../..
 
@@ -17,21 +63,18 @@ cd ../..
 
 echo "####### PREPARING VLC BUILD ENV ########"
 
-./bootstrap
-mkdir -p vlc-build-davinci/staging
-cd vlc-build-davinci
+if [ ! -f configure ] ; then
+   ./bootstrap
+   exit 0
+fi
 
 echo "####### BUILDING VLC ########"
 
-../build-vlc.sh
-
-echo "####### INSTALLING VLC AND CLEANING CRUFT ########"
-
-make DESTDIR=`pwd`/staging install
-find staging -name *.so -exec arm-linux-strip --strip-unneeded \{\} \;
-find staging -name *.a | xargs rm -f;
-arm-linux-strip --strip-unneeded staging/usr/bin/vlc
-rm -rf staging/usr/share
-
-cp -a staging/* ${INSTALL_MOD_PATH}/
+mkdir -p vlc-build-davinci/staging
+cd vlc-build-davinci
+if [ ! -f Makefile ] ; then
+   ../configure-neuros.sh
+fi
+make
+install_vlc
 
